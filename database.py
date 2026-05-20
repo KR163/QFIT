f65cbc52a7476c88b2051e54eacbf68f096a006d
KR163 <129912142+KR163@users.noreply.github.com>
2026-05-21 01:01:32 +0300

    Update database.py

--- database.py
+++ database.py
@@ -3,16 +3,19 @@ import sqlite3
 def init_db():
     conn = sqlite3.connect('fitness_system.db')
     c = conn.cursor()
-    # جدول المستخدمين
-    c.execute('''CREATE TABLE IF NOT EXISTS users 
-                 (code TEXT PRIMARY KEY, name TEXT, goal TEXT)''')
+    # الهيكل الجديد ليدعم البيانات والتمارين
+    c.execute('DROP TABLE IF EXISTS users')
+    c.execute('''CREATE TABLE users 
+                 (code TEXT PRIMARY KEY, name TEXT, goal TEXT, 
+                  sat TEXT, sun TEXT, mon TEXT, tue TEXT, wed TEXT, thu TEXT, fri TEXT)''')
     conn.commit()
     conn.close()
 
-def add_user(code, name, goal):
+def add_user(code, name, goal, sat, sun, mon, tue, wed, thu, fri):
     conn = sqlite3.connect('fitness_system.db')
     c = conn.cursor()
-    c.execute("INSERT OR REPLACE INTO users VALUES (?, ?, ?)", (code, name, goal))
+    c.execute("INSERT OR REPLACE INTO users VALUES (?,?,?,?,?,?,?,?,?,?)", 
+              (code, name, goal, sat, sun, mon, tue, wed, thu, fri))
     conn.commit()
     conn.close()
 
@@ -21,5 +24,13 @@ def get_all_users():
     c = conn.cursor()
     c.execute("SELECT * FROM users")
     data = c.fetchall()
+    conn.close()
+    return data
+
+def get_user(code):
+    conn = sqlite3.connect('fitness_system.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM users WHERE code = ?", (code,))
+    data = c.fetchone()
     conn.close()
     return data
